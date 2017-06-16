83486186a9b4e6c1041a2bc5c21f3e73281cd119
Aytekin Yaliz <aytekinyaliz@gmail.com>
2017-06-16 16:40:49 +0100

    Update index.js

--- dist/controllers/index.js
+++ dist/controllers/index.js
@@ -16,7 +16,7 @@ class IndexRoute {
         this.router.get('/', (req, res, next) => {
             res.json({
                 version: packageJson.version,
-                NODE_ENV: JSON.parse(process.env),
+                NODE_ENV: process.env.NODE_ENV,
                 env: process.env
             });
         });
