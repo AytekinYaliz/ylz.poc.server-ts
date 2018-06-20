bed4153598e36a051948c241313cadece6ba0f2f
Aytekin Yaliz <aytekinyaliz@gmail.com>
2018-06-20 16:40:19 +0100

    Update copyStaticAssets.js

--- copyStaticAssets.js
+++ copyStaticAssets.js
@@ -3,3 +3,5 @@ var shell = require('shelljs');
 shell.cp('-R', 'src/public/js/lib', 'dist/public/js/lib');
 shell.cp('-R', 'src/public/fonts', 'dist/public/fonts');
 shell.cp('-R', 'src/public/images', 'dist/public/images');
+shell.cp('-R', 'docs', 'dist');
+shell.cp('-R', 'styles', 'dist');
